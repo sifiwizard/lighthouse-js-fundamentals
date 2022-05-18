2cb3880584280299787e68292706416b81190881
Aidan Prior <sifiwizard.code@gmail.com>
2022-05-18 16:57:35 -0600

    Choo Choo

---
+++ choose-stations.js
@@ -0,0 +1,17 @@
+function chooseStations(stations) {
+  let pollStations = [];
+  for (const station of stations) {
+    if (station[1] >= 20 && (station[2] === 'school' || station[2] === 'community centre')) {
+      pollStations.push(station[0]);
+    }
+  }
+  return pollStations;
+}
+
+const stations = [
+  ['Big Bear Donair', 10, 'restaurant'],
+  ['Bright Lights Elementary', 50, 'school'],
+  ['Moose Mountain Community Centre', 45, 'community centre']
+];
+
+console.log(chooseStations(stations));
