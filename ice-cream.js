30b5f7606a9daf56928b5bf494366437a10934b3
Aidan Prior <sifiwizard.code@gmail.com>
2022-05-19 14:34:38 -0600

    Ice Cream

---
+++ ice-cream.js
@@ -0,0 +1,11 @@
+let iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
+console.log(iceCreamFlavours);
+
+iceCreamFlavours.push('root beer');
+console.log(iceCreamFlavours);
+
+console.log(iceCreamFlavours[0]);
+
+console.log(iceCreamFlavours.slice(-1)[0]);
+
+console.log(iceCreamFlavours.length);
