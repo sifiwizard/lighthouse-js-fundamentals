9bb24ccde23f958eb6c2685ee64e1244ad4a650c
Aidan Prior <sifiwizard.code@gmail.com>
2022-05-19 15:39:14 -0600

    Behold the bad version that is worse than the first one becuase i can't use indexOf. So many lines could be removed if i didn't have to

--- last-index.js
+++ last-index.js
@@ -1,6 +1,12 @@
 function lastIndexOf(array,num) {
-  let index = array.reverse().indexOf(num);
-  return index < 0 ? index : -index + array.length -1;
+  let index = array.length -1;
+  while (index >= 0){
+    if (array[index] == num){
+      return index;
+    }
+    index --;
+  }
+  return index;
 }
 
-console.log(lastIndexOf([], 3));
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 2));
